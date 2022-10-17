d6f0c5237a6a8d3d336dce3bbb3a8268fcb1b8ea
Vasilis Chrysanthou <chrysanthouv95@gmail.com>
2022-10-17 21:02:04 +0100

    Run black

--- src/cards.py
+++ src/cards.py
@@ -1,14 +1,15 @@
-
 class Cards:
     """
     This is the generic class for all Cards, which all other cards will inherit from.
     """
+
     def __init__(self):
         self.discarded = False
         self.getid()
-    
+
     def getid(self):
         pass
+
 
 class ExplodingKitten(Cards):
     def getid(self):
@@ -17,12 +18,14 @@ class ExplodingKitten(Cards):
     def __repr__(self):
         return "Exploding Kitten"
 
+
 class Defuse(Cards):
     def getid(self):
         self.id = 1
 
     def __repr__(self):
         return "Defuse"
+
 
 class Catermelon(Cards):
     def getid(self):
@@ -31,19 +34,22 @@ class Catermelon(Cards):
     def __repr__(self):
         return "Catermelon"
 
+
 class HairyPotatoCat(Cards):
     def getid(self):
         self.id = 3
 
     def __repr__(self):
         return "Hairy Potato Cat"
-    
+
+
 class RainbowCat(Cards):
     def getid(self):
         self.id = 4
-    
+
     def __repr__(self):
         return "Rainbow Cat"
+
 
 class BeardCat(Cards):
     def getid(self):
@@ -52,13 +58,15 @@ class BeardCat(Cards):
     def __repr__(self):
         return "Beard Cat"
 
-class Tacocat(Cards):    
+
+class Tacocat(Cards):
     def getid(self):
         self.flavortext = "I'm a Palindrome"
         self.id = 6
-    
+
     def __repr__(self):
         return "Tacocat"
+
 
 class Favor(Cards):
     def getid(self):
@@ -67,12 +75,14 @@ class Favor(Cards):
     def __repr__(self):
         return "Favor"
 
+
 class Skip(Cards):
     def getid(self):
         self.id = 8
 
     def __repr__(self):
         return "Skip"
+
 
 class Attack(Cards):
     def getid(self):
@@ -81,6 +91,7 @@ class Attack(Cards):
     def __repr__(self):
         return "Attack"
 
+
 class SeeTheFuture(Cards):
     def getid(self):
         self.id = 10
@@ -88,12 +99,14 @@ class SeeTheFuture(Cards):
     def __repr__(self):
         return "See The Future"
 
+
 class Nope(Cards):
     def getid(self):
         self.id = 11
 
     def __repr__(self):
         return "Nope"
+
 
 class Shuffle(Cards):
     def getid(self):
